089bda1fa61c25b560113df0d26c16d84258077c
mnsp-snoble <snoadmin@msnpartnership.com>
2023-01-05 11:45:19 +0000

    Update dev001.ps1

--- dev001.ps1
+++ dev001.ps1
@@ -5,5 +5,3 @@ Write-Host "email from rundeck vault    :" $email
 Write-Host "Api key from rundeck vault  :" $GLPIapiAppToken
 Write-Host "Api user from rundeck vault :" $GLPIuserApiToken
 
-#test 
-#test2
