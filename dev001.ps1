fd2aab12ce70273feca348435db0a22cc8ed1bfd
mnsp-snoble <snoadmin@msnpartnership.com>
2023-01-05 13:29:14 +0000

    begin testing

--- dev001.ps1
+++ dev001.ps1
@@ -6,5 +6,12 @@ Write-Host "Api key from rundeck vault  :" $GLPIapiAppToken
 Write-Host "Api user from rundeck vault :" $GLPIuserApiToken
 Write-Host "GitHub Uri                  :" $GitHubUri
 
-Set-Location $GamDir
+$gamOU = "/ZZ Chrome Devices/Writhlington Secondary" #writhlington MNSP instance
+$gamParams = "cros_ou_and_children ""$gamOu"" print cros fields serialNumber,annotatedAssetId,ou,annotatedLocation,ethernetMacAddress,firmwareVersion,lastEnrollmentTime,lastSync,macAddress,model,notes,osVersion,status,meid,autoUpdateExpiration"
+
 Invoke-Expression "$GamDir\gam.exe info domain" 
+Invoke-Expression "$GamDir\gam.exe $gamParams" | out-file -FilePath $tempcsv -ErrorAction Continue #get all chromeOS devices from google workspace
+
+$GsuiteChromeDevices = @()
+$GsuiteChromeDevices = Import-Csv -Path $tempcsv #create array of all found Gsuite chrome devices
+$GsuiteChromeDevices.Count
