ce2df2244a693b8eaf8cf83ea3969264833625df
mnsp-snoble <snoadmin@msnpartnership.com>
2023-01-05 11:55:39 +0000

    Update dev001.ps1

--- dev001.ps1
+++ dev001.ps1
@@ -5,3 +5,6 @@ Write-Host "email from rundeck vault    :" $email
 Write-Host "Api key from rundeck vault  :" $GLPIapiAppToken
 Write-Host "Api user from rundeck vault :" $GLPIuserApiToken
 Write-Host "GitHub Uri                  :" $GitHubUri
+
+Set-Location $GamDir
+Invoke-Expression "$GamDir\gam.exe info domain" 
