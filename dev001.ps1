ff8d6b1725bd4b935f6694ffdc04e444b91b636c
mnsp-snoble <snoadmin@msnpartnership.com>
2023-01-05 13:38:51 +0000

    Update dev001.ps1

--- dev001.ps1
+++ dev001.ps1
@@ -15,3 +15,22 @@ Invoke-Expression "$GamDir\gam.exe $gamParams" | out-file -FilePath $tempcsv -Er
 $GsuiteChromeDevices = @()
 $GsuiteChromeDevices = Import-Csv -Path $tempcsv #create array of all found Gsuite chrome devices
 $GsuiteChromeDevices.Count
+
+$SessionToken = Invoke-RestMethod -Verbose "$AppURL/initSession" -Method Get -Headers @{"Content-Type" = "application/json";"Authorization" = "user_token $UserToken";"App-Token"=$AppToken}
+#https://www.urldecoder.org/
+
+$SearchResultApi="?is_deleted=0&as_map=0&range=0-100000000&browse=0&criteria[0][link]=AND&criteria[0][field]=4&criteria[0][searchtype]=equals&criteria[0][value]=14&criteria[1][link]=OR&criteria[1][field]=4&criteria[1][searchtype]=equals&criteria[1][value]=15&itemtype=Computer&start=0"
+$SearchResult=@()
+#glpi device types 14 and 15 : chromebooks and chromeflexos
+$SearchResult = Invoke-RestMethod "$AppURL/search/Computer$SearchResultApi" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
+
+$uuids=@()
+$uuids = $SearchResult.data.1 # create uuids array from api returned results
+
+#discovered glpi devices:
+$uuids.count
+
+
+
+#close api session...
+Invoke-RestMethod "$AppURL/killSession" -Headers @{"session-token"=$SessionToken.session_token; "App-Token" = "$AppToken"}
